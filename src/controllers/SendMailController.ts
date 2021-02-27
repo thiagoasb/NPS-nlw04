ffa0b772c1a167cf2ef4fe10b567bddc00fdeeb4
thiagoasb <thiagoasbezerra@gmail.com>
2021-02-27 10:51:36 -0300

    Recebimento da resposta e controller do calculo do NPS

--- src/controllers/SendMailController.ts
+++ src/controllers/SendMailController.ts
@@ -31,22 +31,23 @@ class SendMailController {
             })
         }
     
+    const npsPath = resolve(__dirname, "..", "views", "emails", 'npsMail.hbs');
+    
+    const surveyUserAlreadyExists = await surveysUsersRepository.findOne({
+        where: {user_id: user.id, value: null}, //condição de AND
+        relations: ["user", "survey"]
+    });
+
     const variables = {
         name: user.name,
         title: survey.title,
         description: survey.description,
-        user_id: user.id,
+        id: "",
         link: process.env.URL_MAIL,
     }
-    
-    const npsPath = resolve(__dirname, "..", "views", "emails", 'npsMail.hbs');
-    
-    const surveyUserAlreadyExists = await surveysUsersRepository.findOne({
-        where: [{user_id: user.id}, {value: null}],
-        relations: ["user", "survey"]
-    });
 
     if(surveyUserAlreadyExists) {
+        variables.id = surveyUserAlreadyExists.id;
         await SendMailService.execute(email, survey.title, variables, npsPath);
         return response.json(surveyUserAlreadyExists);
     }
@@ -55,9 +56,10 @@ class SendMailController {
         user_id: user.id,
         survey_id
     });
-
     
     await surveysUsersRepository.save(surveyUser);
+
+    variables.id = surveyUser.id;
     
     await SendMailService.execute(email, survey.title, variables, npsPath);
 
